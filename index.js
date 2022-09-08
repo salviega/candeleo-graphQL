7350d25ffd4d92b231def8271526a518d3ef1520
Santiago <salviega6@gmail.com>
2022-09-08 08:59:02 -0500

    [FEAT] Preparing project for deployment

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const typeDefs = require('./lib/schema')
 const resolvers = require('./lib/resolvers')
 
 const server = new ApolloServer({ typeDefs, resolvers })
-server.listen().then(({ url }) => {
-  console.log(`Server ready at ${url}`)
+server.listen({port: process.env.PORT || 4000}).then(({ url, port }) => {
+  console.log(
+    `Server is running
+    Listening on port ${port}
+    Query at ${url}`)
 })
